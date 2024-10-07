8bff2166830696b8964226b90ac86d5e97754d9c
Abdullahsabir123 <abdullahsabir558@gmail.com>
2024-10-07 12:45:12 +0500

    Incomplete

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Nav from './components/Nav'
 import Herosection from './components/Herosection'
 import Footer from './components/Footer'
+import About from './components/About'
 
 export default function App() {
   return (
     <React.StrictMode>
       <Nav Brand='Transport' pic='https://images.pexels.com/photos/3207442/pexels-photo-3207442.jpeg?auto=compress&cs=tinysrgb&w=600'/>
       <Herosection/>
+      <About/>
       <Footer/>
     </React.StrictMode>
   )
---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './About.css'
+
+export default function About() {
+  return (
+    <>
+    <div className='container'>
+        <div className='container1'>
+            <h2 className='text'>About</h2>
+            <h3 className='text'>Who we are</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque a commodi tempore facilis at quo, aspernatur enim doloremque inventore.</p>
+            <ol>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit Quas</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit Quas</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit Quas</li>
+            </ol>
+        </div>
+        <div className='container2'></div>
+    </div>
+    </>
+  )
+}
