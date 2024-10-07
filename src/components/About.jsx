7db777f913497c3ca2aff47f21e24cd3cd6e754b
Abdullahsabir123 <abdullahsabir558@gmail.com>
2024-10-07 13:13:06 +0500

    Completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,18 +4,24 @@ import './About.css'
 export default function About() {
   return (
     <>
+    <center>
+    <h1 className='text1'>About</h1>
+    </center>
     <div className='container'>
         <div className='container1'>
-            <h2 className='text'>About</h2>
             <h3 className='text'>Who we are</h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque a commodi tempore facilis at quo, aspernatur enim doloremque inventore.</p>
+            <p className='mar'>Lorem ipsum dolor sit amet consectetur adipisicing elit Eaque a commodi</p>
+            <p>tempore facilis at quo aspernatur enim doloremque inventore</p>
             <ol>
                 <li>Lorem ipsum dolor sit amet consectetur adipisicing elit Quas</li>
                 <li>Lorem ipsum dolor sit amet consectetur adipisicing elit Quas</li>
                 <li>Lorem ipsum dolor sit amet consectetur adipisicing elit Quas</li>
             </ol>
         </div>
-        <div className='container2'></div>
+        <div className='container2'>
+            <p className='mar1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit minus a quis, iusto enim, reprehenderit architecto ipsa veritatis corporis dolorum sint quos eveniet repudiandae, quibusdam veniam. Excepturi recusandae eius facere fuga doloremque quis, assumenda, culpa, ratione consectetur aut ipsa dolores modi a!</p>
+            <button> Read More <i class="fa-solid fa-arrow-right-long"></i></button>
+        </div>
     </div>
     </>
   )
